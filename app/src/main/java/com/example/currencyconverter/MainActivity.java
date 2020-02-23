acda6c3dc1da671705e7a91ec837431b0731c445
Nima Thakare <nima.thakare@gmail.com>
2020-02-23 11:31:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText Amount_editText;
+    public TextView InRupees_editText;
+
+    public void convertCurrency(View view){
+
+        Log.i("Info","Button Pressed");
+
+        EditText Amount_editText = (EditText)findViewById(R.id.Amount_editText);
+        TextView InRupees_editText = (TextView) findViewById(R.id.InRupees_editText);
+
+        String amountInYen = Amount_editText.getText().toString();
+
+        double amountInYenDouble = Double.parseDouble(amountInYen);
+
+        double amountInRupeesDouble = amountInYenDouble * 0.65;
+
+        String amountInRupeesDoubleString = Double.toString(amountInRupeesDouble);
+
+        InRupees_editText.setText(amountInRupeesDoubleString);
+
+        Log.i("amount in rupees",amountInRupeesDoubleString);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*double amountInRupeesDouble = 0;
+        double InRupees_editText=amountInRupeesDoubleString.setText().toString();*/
+    }
+
+
+}
